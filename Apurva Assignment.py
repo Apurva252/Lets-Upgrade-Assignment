79286276b1ad98a3995206ac356e901fcad8a217
Apurva252 <143873822+Apurva252@users.noreply.github.com>
2023-09-02 14:41:00 +0530

    Add files via upload

---
+++ Apurva Assignment.py
@@ -0,0 +1,8 @@
+a = int(input("A"))
+b = int(input("B"))
+print("Q1.Floor Division")
+print("Floor division is",a//b)
+print("Q2.a power b")
+print("A Power B is",a**b)
+print("Q3.Find out whether two variables have equal value or not- if yes then print True if they are not equal then print False")
+print("The statement is",a==b)
